cb77170656c8e710ab15192abf2b023ec1c5412b
DrNopt <edocpt@gmail.com>
2026-06-07 13:06:44 +0100

    Update start-gatewayrs.sh

--- hm-gatewayrs/start-gatewayrs.sh
+++ hm-gatewayrs/start-gatewayrs.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 echo "Starting start-gatewayrs.sh"
-rm -f settings.toml
+
+echo "=== default.toml contents ==="
+cat /etc/helium_gateway/default.toml
+echo "=============================="
+
 rm -f /etc/helium_gateway/settings.toml
 
 echo "Setting REGION_OVERRIDE"
 if [[ -v REGION_OVERRIDE ]]
 then
   echo "REGION_OVERRIDE is set to ${REGION_OVERRIDE}"
-  echo 'region = "'"${REGION_OVERRIDE}\"" >> settings.toml
+  echo 'region = "'"${REGION_OVERRIDE}\"" >> /etc/helium_gateway/settings.toml
 else
   echo "REGION_OVERRIDE not set"
   exit 1
 fi
 
 echo "Using file-based keypair"
-echo 'keypair = "/var/data/gateway_key.bin"' >> settings.toml
-
-cat /etc/helium_gateway/settings.toml.template >> settings.toml
-cp settings.toml /etc/helium_gateway/settings.toml
+echo 'keypair = "/var/data/gateway_key.bin"' >> /etc/helium_gateway/settings.toml
 
 echo "=== settings.toml contents ==="
 cat /etc/helium_gateway/settings.toml
 echo "=============================="
 
 echo "Calling helium_gateway server ..."
-/usr/bin/helium_gateway -c /etc/helium_gateway/settings.toml server &
+/usr/bin/helium_gateway server &
 
 sleep 5
 echo "Checking key info..."
-if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway/settings.toml key info)
+if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway key info)
 then
   echo "Can't get miner key info"
 else
